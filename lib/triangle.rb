e6670c47e46771eecd19bfb38b280d4878903aa7
Morgan Fudge <mfudge92@gmail.com>
2017-11-14 04:16:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
   end
 
   def kind
+    raise TriangleError if @s1 == 0 or @s2 == 0 pr @s3 == 0
     return :equilateral if @s1 == @s2 and @s1 == @s3
     return :isosceles if @s1 == @s2 or @s1 == @s3 or @s2 == @s3
     return :scalene if @s1 != @s2 or @s2 != @s3
