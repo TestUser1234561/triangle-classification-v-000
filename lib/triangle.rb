4cc3bc83cc0bd685b7cb044a5de3526b0ea2c8fe
Morgan Fudge <mfudge92@gmail.com>
2017-11-14 04:11:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,11 @@ class Triangle
     @s3 = s3
   end
 
+  def kind
+    return :equilateral if @s1 = @s2 = @s3
+    return :isoceles if @s1 = @s2 or @s1 = @s3 or @s2 = @s3
+  end
+
 end
 
 class TriangleError < StandardError
